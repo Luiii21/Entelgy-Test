59247b4c861591d8ad1322f09a0efbc05c083b03
Jeanlui21 <pamui_2121@hotmail.com>
2021-02-20 15:23:52 -0500

    principal function completed

--- components/countries.js
+++ components/countries.js
@@ -1,8 +1,37 @@
-const url = 'https://restcountries.eu/rest/v2/lang/es'
+class countryCard extends HTMLElement {
+    constructor() {
+        super();
+    }
 
-fetch(url)
-    .then(response => response.json())
-    .then(data => {
-        console.log(data)
-    })
-    .catch(err => console.log(err));
+    connectedCallback() {
+        this.innerHTML = `<div class="content-countries" id="content-countries"></div>`;
+        const url = 'https://restcountries.eu/rest/v2/lang/es'
+
+        fetch(url)
+            .then(response => response.json())
+            .then(data => {
+                if (data.length > 0) {
+                    let box = '';
+                    let difference = 0;
+                    if (data.length > 12) {
+                        difference = data.length - 12
+                    }
+                    data.slice(difference).forEach((u) => {
+                        box += `<div class="country shadow-sm">`
+                        box += `<div><span>` + "Nombre: " + u.name + `</span> <br>`
+                        box += `<span>` + "Población: " + u.population + `</span> <br>`
+                        box += `<span>` + "Capital: " + u.capital + `</span></div>`
+                        box += `</div>`
+                    })
+                    let element = document.getElementById('content-countries')
+                    element.innerHTML = box;
+                }
+
+            })
+            .catch(err => console.log(err));
+    }
+}
+
+window.customElements.define("app-country-card", countryCard);
+
+
