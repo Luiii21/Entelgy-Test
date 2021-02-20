1bf0c41cf56ddbd964862cee9719fe97ed990a37
Jeanlui21 <pamui_2121@hotmail.com>
2021-02-20 12:18:30 -0500

    first commit

---
+++ components/countries.js
@@ -0,0 +1,8 @@
+const url = 'https://restcountries.eu/rest/v2/lang/es'
+
+fetch(url)
+    .then(response => response.json())
+    .then(data => {
+        console.log(data)
+    })
+    .catch(err => console.log(err));
